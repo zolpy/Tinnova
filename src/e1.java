ffa508a14da89e77b9a62940f42b41596d6557d8
Brandão <localcidadao@bol.com.br>
2020-07-08 11:39:40 -0300

    exercicio 1

--- src/e1.java
+++ src/e1.java
@@ -1,32 +1,29 @@
 public class e1 {
-    int total_de_eleitores = 1000;
-    int validos = 800;
-    int votos_brancos = 150;
-    int nulos =50;
+//    int total_de_eleitores = 1000;
+//    int validos = 800;
+//    int votos_brancos = 150;
+//    int nulos =50;
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        e1 validos = new e1();
-        validos.percValidos(800, 1000);
+        percValidos(800, 1000);
 
-        e1 brancos = new e1();
-        brancos.percBrancos(150, 1000);
+        percBrancos(150, 1000);
 
-        e1 nulos = new e1();
-        nulos.percNulos(50, 1000);
+        percNulos(50, 1000);
     }
 
 
-    void percValidos(int num1, int num2){
-        System.out.println("O percentual de votos válidos em relação ao total de eleitores é:  "+ (validos  / total_de_eleitores));
+    private static void percValidos(int num1, int num2) {
+        System.out.println("O percentual de votos válidos em relação ao total de eleitores é:  " + (num1 / num2));
     }
 
-    void percBrancos (int num1, int num2){
-        System.out.println("O percentual de votos em brancos em relação ao total de eleitores é: " + (votos_brancos   / total_de_eleitores));
+    private static void percBrancos(int num1, int num2) {
+        System.out.println("O percentual de votos em brancos em relação ao total de eleitores é: " + (num1 / num2));
     }
 
-    void percNulos (int num1, int num2){
-        System.out.println("O percentual de nulos em relação ao total de eleitores é: "+ (nulos   / total_de_eleitores));
+    private static void percNulos(int num1, int num2) {
+        System.out.println("O percentual de nulos em relação ao total de eleitores é: " + (num1 / num2));
     }
 
 
